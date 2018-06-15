e60c4c64df3c0843315e5d7509d4ce4f16509887
Jose A. Catalán <yo@joseacat.io>
2018-06-15 13:59:09 +0200

    Commit inicial
    Añade los distritos de Portugal a Magento 1.9

---
+++ anadir_distritos_portugal.sql
@@ -0,0 +1,40 @@
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Aveiro', 'Aveiro');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Beja', 'Beja');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Braga', 'Braga');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Bragança', 'Bragança');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Castelo Branco', 'Castelo Branco');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Coimbra', 'Coimbra');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Évora', 'Évora');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Faro', 'Faro');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Guarda', 'Guarda');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Leiria', 'Leiria');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Lisboa', 'Lisboa');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Portalegre', 'Portalegre');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Porto', 'Porto');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Santarém', 'Santarém');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Setúbal', 'Setúbal');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Viana do Castelo', 'Viana do Castelo');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Vila Real', 'Vila Real');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Viseu', 'Viseu');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Açores', 'Açores');
+INSERT INTO `directory_country_region` (`country_id`, `code`, `default_name`) VALUES
+    ('PT', 'Madeira', 'Madeira');
